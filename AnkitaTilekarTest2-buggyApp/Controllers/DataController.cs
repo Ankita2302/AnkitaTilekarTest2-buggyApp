e093888b7290f758fb2fcda7f8d1f7282f0242b5
ankit <ankit@ANKITAT>
2026-02-13 13:42:16 +0530

    Add project files.

---
+++ AnkitaTilekarTest2-buggyApp/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuggyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            string result = "Sample data";
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                return Ok(new { message = "Data fetched" });
+            }
+
+            return BadRequest("No data");
+        }
+    }
+}
---
+++ AnkitaTilekarTest2-buggyApp/Controllers/InvoiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuggyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInvoice()
+        {
+            var items = new List<Item>
+            {
+                new Item { name = "Widget A", price = 19.99 }
+            };
+
+            return Ok(new { items });
+        }
+
+        public class Item
+        {
+            public string name { get; set; }
+            public double price { get; set; }
+        }
+    }
+}
